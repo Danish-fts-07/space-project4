bacd4eb255ba4ee7602d8a2aaebcb6e9fe842c2a
Danish-fts-07 <danishfts31103@gmail.com>
2024-05-12 12:18:46 -0700

    change

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -3,7 +3,8 @@ import "./Home.css"
 function Home() {
   return (
     <>
-<section className='Section-home'>
+<section className='Section-home' >
+  <img src="homeD.jpg" alt="" />
     <div className="home-content">
         <div className="content"><h3>So, YOU  WANT  TO TRAVEL TO</h3><h1> SPACE</h1><p> Let’s face it; if you want to go to space, you might as well <br /> genuinely go to outer space and not hover kind of on the <br /> edge of it. Well sit back, and relax because we’ll give you <br /> a truly out of this world experience!</p></div>
         <div className="explore-div">EXPLORE </div>
