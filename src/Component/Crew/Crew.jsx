d22652a54588683963ec128067d6f5e00d5f415b
Danish-fts-07 <danishfts31103@gmail.com>
2024-05-27 12:08:03 -0700

    set rese

--- src/Component/Crew/Crew.jsx
+++ src/Component/Crew/Crew.jsx
@@ -10,49 +10,49 @@ function Crew() {
       <section className='crew-section'>
         <div className="crew-all">
           <div className="content-nav">
-          <h3>02 MEET YOUR CREW</h3>
-          <div className={crewToggle === 1 ? "crew-content" : "contentC"}>
+            <h3>02 MEET YOUR CREW</h3>
+            <div className={crewToggle === 1 ? "crew-content" : "contentC"}>
               <div className="all-contentC">
-              <div className="name-head">
-                <h4>COMMANDER </h4>
-                <h1>DOUGLAS HURLEY</h1>
+                <div className="name-head">
+                  <h4>COMMANDER </h4>
+                  <h1>DOUGLAS HURLEY</h1>
                 </div>
                 <p>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
               </div>
-              </div>
-          <div className={crewToggle === 2 ? "crew-content" : "contentC"}>
+            </div>
+            <div className={crewToggle === 2 ? "crew-content" : "contentC"}>
               <div className="all-contentC">
-              <div className="name-head">
-                <h4> PILOT</h4>
-                <h1>VICTOR GLOVER</h1>
+                <div className="name-head">
+                  <h4> PILOT</h4>
+                  <h1>VICTOR GLOVER</h1>
                 </div>
                 <p>Victor Glover Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.</p>
               </div>
-              </div>
-          <div className={crewToggle === 3 ? "crew-content" : "contentC"}>
+            </div>
+            <div className={crewToggle === 3 ? "crew-content" : "contentC"}>
               <div className="all-contentC">
-              <div className="name-head">
-                <h4> MISSION SPECIALIST </h4>
-                <h1>MARK SHUTTLEWORTH </h1>
+                <div className="name-head">
+                  <h4> MISSION SPECIALIST </h4>
+                  <h1>MARK SHUTTLEWORTH </h1>
                 </div>
                 <p>Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.</p>
               </div>
-              </div>
+            </div>
             <div className={crewToggle === 4 ? "crew-content" : "contentC"}>
               <div className="all-contentC">
-              <div className="name-head">
-                <h4> FLIGHT ENGINEER</h4>
-                <h1>ANOUSHEH ANSARI</h1>
+                <div className="name-head">
+                  <h4> FLIGHT ENGINEER</h4>
+                  <h1>ANOUSHEH ANSARI</h1>
                 </div>
                 <p>Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.</p>
               </div>
-              </div>
-              <div className="crewNav">
-                <div className="crew-option" onClick={() => { updatecrewToggle(1) }}></div>
-                <div className="crew-option" onClick={() => { updatecrewToggle(2) }}></div>
-                <div className="crew-option" onClick={() => { updatecrewToggle(3) }}></div>
-                <div className="crew-option" onClick={() => { updatecrewToggle(4) }}></div>
-              </div>
+            </div>
+            <div className="crewNav">
+              <div className="crew-option" onClick={() => { updatecrewToggle(1) }}></div>
+              <div className="crew-option" onClick={() => { updatecrewToggle(2) }}></div>
+              <div className="crew-option" onClick={() => { updatecrewToggle(3) }}></div>
+              <div className="crew-option" onClick={() => { updatecrewToggle(4) }}></div>
+            </div>
           </div>
           <div className={crewToggle === 1 ? "crew-content" : "contentC"}>
             <img src="camanderC.webp" alt="" className="crew-img" />
--- src/Component/Tech/Tech.jsx
+++ src/Component/Tech/Tech.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import "./Technology.css"
+import { useState } from 'react'
 function Tech() {
+  const [Toggle, setToggle] = useState(1)
+  const Updatetoggle = (id) => {
+    console.log(id)
+    setToggle(id)
+  }
   return (
-<>
-<section className='technology-section'></section>
-</>
+    <>
+      <section className='technology-section'>
+        <div className="tech-allT">
+          <div className="nav-headingT">
+            <h3 >03 SPACE LAUNCH 101</h3>
+            <div className="nav-contentT">
+              <div className="tech-navT">
+                <div className={"nav-optionT"} onClick={() => { Updatetoggle(1) }}>1</div>
+                <div className={"nav-optionT"} onClick={() => { Updatetoggle(2) }}>2</div>
+                <div className={"nav-optionT"} onClick={() => { Updatetoggle(3) }}>3</div>
+              </div>
+              <div className={Toggle === 1 ? "show-contentT" : "contentT"}>
+                <div className="all-contentT">
+                  <h4>THE TERMINOLOGY...</h4>
+                  <h1>LAUNCH VEHICLE</h1>
+                  <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+                </div>
+              </div>
+              <div className={Toggle === 2 ? "show-contentT" : "contentT"}>
+                <div className="all-contentT">
+                  <h4>THE TERMINOLOGY...</h4>
+                  <h1> SPACEPORT </h1>
+                  <p>A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.</p>
+                </div>
+              </div>
+              <div className={Toggle === 3 ? "show-contentT" : "contentT"}>
+                <div className="all-contentT">
+                  <h4>THE TERMINOLOGY...</h4>
+                  <h1>SPACE CAPSULE</h1>
+                  <p>A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={Toggle === 1 ? "show-contentT" : "contentT"}>
+          <img src="tech1D.jpg" alt="" id='imgT' />
+          </div>
+          <div className={Toggle === 2 ? "show-contentT" : "contentT"}>
+          <img src="techD.jpg" alt="" id='imgT' />
+          </div>
+          <div className={Toggle === 3 ? "show-contentT" : "contentT"}>
+          <img src="tech2D.jpg" alt="" id='imgT' />
+          </div>
+         
+        </div>
+      </section>
+    </>
   )
 }
 
